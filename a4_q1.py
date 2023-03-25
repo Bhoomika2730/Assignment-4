f9eb7574ed8c695bed99fac104101a7c750ebe15
Bhoomika2730 <125494668+Bhoomika2730@users.noreply.github.com>
2023-03-25 00:26:21 -1200

    Add files via upload

---
+++ a4_q1.py
@@ -0,0 +1,5 @@
+#Write a Python program to create a lambda function that adds 25 to a given number passed in as an argument.
+
+result = lambda x: x + 25
+summation_result = result(10)
+print(summation_result)
